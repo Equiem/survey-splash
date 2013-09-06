2b5ca75de6679cc21f9ed9000abf007e611543f6
Suryadi <suryadi.lawira@equiem.com.au>
2013-09-06 13:31:30 +1000

    move Jplayer init to js

--- js/main.js
+++ js/main.js
@@ -63,6 +63,26 @@ $(function() {
     pagination: "#slideshownav"
   });
 
+  $("#jquery_jplayer_1").jPlayer({
+    ready: function () {
+      $(this).jPlayer("setMedia", {
+        m4v: "http://www.jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v",
+        ogv: "http://www.jplayer.org/video/ogv/Big_Buck_Bunny_Trailer.ogv",
+        webmv: "http://www.jplayer.org/video/webm/Big_Buck_Bunny_Trailer.webm",
+        poster: "http://www.jplayer.org/video/poster/Big_Buck_Bunny_Trailer_480x270.png"
+      });
+    },
+    swfPath: "js",
+    supplied: "webmv, ogv, m4v",
+    size: {
+      width: "640px",
+      height: "360px",
+      cssClass: "jp-video-360p"
+    },
+    smoothPlayBar: true,
+    keyEnabled: true
+  });
+
   $("#playmobile").click(function(e) {
     e.preventDefault();
     $("#jquery_jplayer_1").jPlayer("play");
