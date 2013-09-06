e0605d214f17df150f8a8d9f0bc570f8d6ccc14e
Suryadi <suryadi.lawira@equiem.com.au>
2013-09-06 15:44:22 +1000

    fix the url for the twitter and linked in change config player dynamic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
+// video config url
+window.video_config = {
+  // poster : 'http://www.jplayer.org/video/poster/Big_Buck_Bunny_Trailer_480x270.png',
+  // m4v    : 'http://www.jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v',
+  // ogv    : 'http://www.jplayer.org/video/ogv/Big_Buck_Bunny_Trailer.ogv',
+  // webmv  : 'http://www.jplayer.org/video/webm/Big_Buck_Bunny_Trailer.webm'
+
+  poster : 'http://www.jplayer.org/video/poster/Big_Buck_Bunny_Trailer_480x270.png',
+  m4v    : 'http://equiem.github.io/survey-splash/video/intro.mp4',
+  ogv    : 'http://equiem.github.io/survey-splash/video/intro.ogv',
+  webmv  : 'http://equiem.github.io/survey-splash/video/intro.webm'
+};
+
 $(function() {
   // READ THIS :
   // Please replace the meta tag title and description and heading link in
   // the html file, so at least google can index it!
-  // CHECK THE VIDEO URL AS WELL IN HTML
   var config = {
     url           : "http://60castlereagh.tenantportal.com.au",
     building_name : "60 Castlereagh",
@@ -18,8 +30,8 @@ $(function() {
       e_title    = encodeURIComponent(config.social_title);
 
   $("#fb").attr("href", 'http://www.facebook.com/sharer.php?s=100&p[title]=' + e_building + '&p[url]=' + e_url + '&p[summary]=' + e_title);
-  $("#li").attr("href", 'https://twitter.com/share?url=' + e_url+ '&text=' + e_title);
-  $("#tw").attr("href", 'http://www.linkedin.com/shareArticle?mini=true&url=' + e_url+ '&title=' + e_title);
+  $("#li").attr("href", 'http://www.linkedin.com/shareArticle?mini=true&url=' + e_url+ '&title=' + e_title);
+  $("#tw").attr("href", 'https://twitter.com/share?url=' + e_url+ '&text=' + e_title);
   $("#mg").attr("href", 'mailto:?body=' + e_url+ '&subject=' + e_title);
 
   // --------------------------------------------------
