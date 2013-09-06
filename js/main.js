503ffad18c1cd16b8a25bd9d2fa2da92473f358a
Suryadi <suryadi.lawira@equiem.com.au>
2013-09-06 10:38:28 +1000

    test swf path

--- js/main.js
+++ js/main.js
@@ -73,6 +73,7 @@ $(function() {
         poster: "http://www.jplayer.org/video/poster/Big_Buck_Bunny_Trailer_480x270.png"
       });
     },
+    swfPath: "js/Jplayer.swf",
     supplied: "webmv, ogv, m4v",
     size: {
       width: "640px",
